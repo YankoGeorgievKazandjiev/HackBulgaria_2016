fcd9e59c9b2d3ffc2a2c85df4b8bd4068d9e1969
YankoGeorgievKazandjiev <janko.g.kazandjiev@abv.bg>
2017-02-02 15:36:42 +0200

    Update anagrams.py

--- week_1/Extra-Tasks/Anagrams/anagrams.py
+++ week_1/Extra-Tasks/Anagrams/anagrams.py
@@ -1,1 +1,7 @@
-
+# Task: Is two words anagrams?
+def is_anagram(word_one, word_two):
+    sorted_word_one = sorted(word_one.lower())
+    sorted_word_two = sorted(word_two.lower())
+    if sorted_word_one == sorted_word_two:
+        return True
+    return False
